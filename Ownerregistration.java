030c50fccdf32c75733813a64e193ed3426212dd
Rahul Yadav <Rahul Yadav>
2017-06-19 19:09:21 +0530

    Adding more attributes of rooms and individual rents

--- Ownerregistration.java
+++ Ownerregistration.java
@@ -13,7 +13,12 @@ public class Ownerregistration {
 	private JPasswordField confirmpassword;
 	private JTextField rooms;
 	private JTextArea address;
-	private JTextField rent;
+	private JTextField rent1;
+	private JTextField rent2;
+	private JTextField rent3;
+	private JTextField rent4;
+	private JTextField roomwidth;
+	private JTextField roomheight;
 	private JButton submit;
 	int test = 5;
 	public void create(){
@@ -31,8 +36,18 @@ public class Ownerregistration {
 		rooms = new JTextField();
 		JLabel Address = new JLabel("Address");
 		address = new JTextArea();
-		JLabel Rent = new JLabel("Rent");
-		rent = new JTextField();
+		JLabel Rent1 = new JLabel("RentofRoom1");
+		rent1 = new JTextField();
+		JLabel Rent2 = new JLabel("RentofRoom1");
+		rent2 = new JTextField();
+		JLabel Rent3 = new JLabel("RentofRoom1");
+		rent3 = new JTextField();
+		JLabel Rent4 = new JLabel("RentofRoom1");
+		rent4 = new JTextField();
+		JLabel room_width = new JLabel("RoomWidth");
+		roomwidth = new JTextField();
+		JLabel room_height = new JLabel("RoomHeight");
+		roomheight = new JTextField();
 		submit = new JButton("SUBMIT");
 		
 		UserName.setBounds(50,30,70,10);
@@ -47,9 +62,19 @@ public class Ownerregistration {
 		rooms.setBounds(140,110,150,15);
 		Address.setBounds(50,130,70,10);
 		address.setBounds(140,130,150,15);
-		Rent.setBounds(50,150,70,10);
-		rent.setBounds(140,150,150,15);
-		submit.setBounds(200,250, 100, 30);
+		Rent1.setBounds(50,150,70,10);
+		rent1.setBounds(140,150,150,15);
+		Rent2.setBounds(50,170,70,10);
+		rent2.setBounds(140,170,150,15);
+		Rent3.setBounds(50,190,70,10);
+		rent3.setBounds(140,190,150,15);
+		Rent4.setBounds(50,210,70,10);
+		rent4.setBounds(140,210,150,15);
+		room_width.setBounds(50, 230, 70, 10);
+		roomwidth.setBounds(140, 230, 150, 15);
+		room_height.setBounds(50, 250, 70, 10);
+		roomheight.setBounds(140, 250, 150, 15);
+		submit.setBounds(200,270, 100, 30);
 		
 		register.add(Name);
 		register.add(name);
@@ -63,8 +88,18 @@ public class Ownerregistration {
 		register.add(rooms);
 		register.add(Address);
 		register.add(address);
-		register.add(Rent);
-		register.add(rent);
+		register.add(Rent1);
+		register.add(rent1);
+		register.add(Rent2);
+		register.add(rent2);
+		register.add(Rent3);
+		register.add(rent3);
+		register.add(Rent4);
+		register.add(rent4);
+		register.add(room_width);
+		register.add(roomwidth);
+		register.add(room_height);
+		register.add(roomheight);
 		register.add(submit);
 		register.setSize(width,height);//400 width and 500 height  
 		register.setLayout(null);//using no layout managers  
