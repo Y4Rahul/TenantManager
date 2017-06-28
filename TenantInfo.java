e1762c924b3e64581e81e6bb31e37fb341d6de73
Rahul Yadav <Rahul Yadav>
2017-06-28 23:09:39 +0530

    Adding action listeners to button to change colour onclick event

--- TenantInfo.java
+++ TenantInfo.java
@@ -1,5 +1,7 @@
 //for inserting TenantInformation
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 public class TenantInfo {  
@@ -7,7 +9,7 @@ public class TenantInfo {
 	{  
 	          	String room="3";
 	          	JFrame f= new JFrame("Tenant Information");
-	          	JPanel panel=new JPanel();  
+	          	JPanel panel=new JPanel();
 	            panel.setBounds(0,0,400,50); 
 	            panel.setBackground(Color.gray);
 	          	JButton[] Room = new JButton[Integer.parseInt(room)];
@@ -15,12 +17,20 @@ public class TenantInfo {
 	            {
 	          	 Room[i] = new JButton("Room"+i);
 	          	 Room[i].setBounds(20+100*i, 20, 100, 50);
+	          	Room[i].addActionListener(new ActionListener() {
+	          	    public void actionPerformed(ActionEvent e) {
+	          	    	System.out.println("room");
+	          	    }          
+	          	 });
 	            }
 	            for(int i=0;i<Integer.parseInt(room);i++)
 	            {
 	          	  panel.add(Room[i]);
 	            }
+	            JButton submit = new JButton("SUBMIT");
+	            submit.setBounds(150,150,100,50);
 	            JPanel P = TenantGui.creategui("rahul","3");
+	            P.add(submit);
 	            f.add(P);
 	            f.add(panel);
 	            f.setSize(400,400);//400 width and 500 height  
