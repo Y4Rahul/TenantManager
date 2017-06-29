345cce1597c29044f81f5755e96d5865de5bc47f
Rahul Yadav <Rahul Yadav>
2017-06-29 22:43:29 +0530

    Adding extra field of information

--- TenantGui.java
+++ TenantGui.java
@@ -27,18 +27,29 @@ public class TenantGui {
         JTextField tenantname = new JTextField();
         JLabel RoomRent = new JLabel("RoomRent");
         JTextField roomrent = new JTextField();
+        JLabel Roomid = new JLabel("RoomId");
+        JTextField roomid = new JTextField();
+        JLabel Contactno = new JLabel("ContactNo");
+        JTextField contactno = new JTextField();
         //JFileChooser file = new JFileChooser();
         
         TenantName.setBounds(50, 70, 100, 20);
         tenantname.setBounds(120, 70, 100, 20);
        RoomRent.setBounds(50, 90, 100, 20);
         roomrent.setBounds(120, 90, 100, 20);
+        Roomid.setBounds(50,110,100,20);
+        roomid.setBounds(120,110,100,20);
+        Contactno.setBounds(50,130,100,20);
+        contactno.setBounds(120,130,100,20);
         //file.setBounds(50,130,100,50);
         
         f.add(TenantName);
         f.add(tenantname);
         f.add(RoomRent);
         f.add(roomrent);
+        f.add(Roomid);
+        f.add(roomid);
+       
         //f.add(file);
        /*f.setSize(400,400);//400 width and 500 height  
 		f.setLayout(null);//using no layout managers  
